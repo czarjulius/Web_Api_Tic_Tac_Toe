9e85dc9747fb451d199b288fb803b98930eafae1
czarjulius <juliusczar.jc@gmail.com>
2020-03-30 13:56:23 +0200

    implemented validation for player

--- spec/web_api/web_api_spec.rb
+++ spec/web_api/web_api_spec.rb
@@ -23,12 +23,12 @@ RSpec.describe App do
   
 
   context "#move" do
-    it "Should return an initial board" do
+    it "Should rreturn a board with initial move" do
       @persist_data.add_detail('player', 'x')
-
+      @persist_data.add_detail('board', ["-","-","-","-","-","-","-","-","-"])
+      
       body={move: 0}.to_json
       post('/move', body, { 'CONTENT_TYPE' => 'application/json' })
-
       expected_result= {message: ["x","-","-","-","-","-","-","-","-"]}.to_json
 
       expect(last_response).to be_ok
@@ -37,7 +37,7 @@ RSpec.describe App do
     end
     it "Should return the updated board" do
       @persist_data.add_detail('player', 'x')
-
+      @persist_data.add_detail('board', ["-","-","-","-","-","-","-","-","-"])
         body={move: 2}.to_json
         post('/move', body, { 'CONTENT_TYPE' => 'application/json' })
 
@@ -50,6 +50,8 @@ RSpec.describe App do
 
     it "Should return the updated board for 'o'" do
       @persist_data.add_detail('player', 'o')
+
+      @persist_data.add_detail('board', ["-","-","-","-","-","-","-","-","-"])
       body={move: 2}.to_json
       post('/move', body, { 'CONTENT_TYPE' => 'application/json' })
 
@@ -62,6 +64,8 @@ RSpec.describe App do
 
   it "Should update the  board after multiple play" do
     @persist_data.add_detail('player', 'o')
+
+    @persist_data.add_detail('board', ["-","-","-","-","-","-","-","-","-"])
     body={move: 2}.to_json
     expected_result= {message: ["-","-","o","-","-","-","-","-","-"]}.to_json
     post('/move', body, { 'CONTENT_TYPE' => 'application/json' })
@@ -86,7 +90,7 @@ RSpec.describe App do
   end
   it "Should win a game by player x" do
     @persist_data.add_detail('player', 'x')
-    @persist_data.add_detail('board', ["-","-","x","-","x","o","x","-","o"])
+    @persist_data.add_detail('board', ["-","-","x","-","x","o","-","-","o"])
     body={move: 6}.to_json
     expected_result= {message: "Player x won the game"}.to_json
     post('/move', body, { 'CONTENT_TYPE' => 'application/json' })
@@ -101,6 +105,26 @@ RSpec.describe App do
     post('/move', body, { 'CONTENT_TYPE' => 'application/json' })
     expect(last_response).to be_ok
     expect(last_response.body).to eq(expected_result)
+  end
+
+  it "Should return an error message that spot is not available" do
+    @persist_data.add_detail('player', 'o')
+    @persist_data.add_detail('board', ["o","x","o","x","o","x","x","-","x"])
+    body={move: 5}.to_json
+    expected_result= {message: "The move spot is taken, play another spot"}.to_json
+    post('/move', body, { 'CONTENT_TYPE' => 'application/json' })
+    expect(last_response).to be_ok
+    expect(last_response.body).to eq(expected_result)
+  end
+end
+
+context "#player" do
+  xit "contains . . ." do
+    body={}.to_json
+    expected_response = {message:'please pass a player field'}
+    post('/player', body, { 'CONTENT_TYPE' => 'application/json' })
+    expect(last_response).to be_ok
+    expect(last_response.body).to eq(expected_response)
   end
 end
   
